4e7e6d46d705bd91fe1f00f5bb1b427653a6f6fd
venesa okuna <venesa.okuna@student.moringaschool.com>
2021-11-24 07:52:56 +0300

    This commit will add spacing and indentation to files.

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ $(document).ready(function(){
       $("#design-image").slideDown('1500').hide('1000');
       $("#design").show('1500');
     });
+
     $("#design").click(function(){
       $("#design").slideUp('1500');
       $("#design-image").slideDown('1500');
@@ -23,6 +24,7 @@ $(document).ready(function(){
       $("#development-image").slideDown('1500').hide('1000');
       $("#development").show('1500');
     });
+
     $("#development").click(function(){
       $("#development").slideUp('1500');
       $("#development-image").slideDown('1500');
@@ -36,6 +38,7 @@ $(document).ready(function(){
       $("#product-image").slideDown('1500').hide('1000');
       $("#product").show('1500');
     });
+    
     $("#product").click(function(){
       $("#product").slideUp('1500');
       $("#product-image").slideDown('1500');
